45761c3fc14c3fbd392e71055ddfa7e361949e95
rjomg <1241718398@qq.com>
2018-03-28 19:42:03 +0800

    配置路由和前后台文件路径

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,18 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+#   前台界面
+Route::group(['prefix' => '/'], function () {
+    Route::get('/', function () {
+        return view('welcome');
+    });
+});
+
+#   后台界面
+Route::group(['prefix' => 'admin'], function () {
+    Route::get('code', 'admin\LoginController@code');//后台验证码
+    //用户登录验证
+    Route::get('login', 'admin\LoginController@showLoginForm')->name('admin.login');
+    Route::post('login', 'admin\LoginController@login');
+    Route::any('logout', 'admin\LoginController@logout');
 });
