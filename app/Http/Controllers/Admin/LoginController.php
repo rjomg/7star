25962cef92279f3634d706e3a815b8295e034b44
rjomg <1241718398@qq.com>
2018-03-29 11:29:50 +0800

    修改LoginController大小写

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Model\Users;
 
-class Logincontroller extends Controller
+class LoginController extends Controller
 {
     //显示后台登录界面
     public function index() {
