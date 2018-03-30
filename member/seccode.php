e281e98ab855c893df223c2c9c357847c5ae73d8
weiki <wen18826478504@163.com>
2018-03-30 10:02:08 +0800

    验证码

--- member/seccode.php
+++ member/seccode.php
@@ -17,12 +17,15 @@ $borderColor = imagecolorallocate($image,0,0,0);
 imagerectangle($image,0,0,89,39,$borderColor);
 
 //填充字符
-$chars = ['零','壹','贰','叁','肆','伍','陆','柒'	,'捌',
-            '玖','一','二','三','四','五','六','七','八','九',1,2,3,4,5,6,7,8,9,0];
+$chars = array('零','壹','贰','叁','肆','伍','陆','柒','捌','玖','一','二','三','四','五','六','七','八','九',1,2,3,4,5,6,7,8,9,0);
 			
 $len = count($chars);
 $char1 = $chars[mt_rand(0,$len)];
+$char2 = "+";
+$char3 = $chars[mt_rand(0,$len)];
 
+$font = dirname(__FILE__). DIRECTORY_SEPARATOR. 'font' . DIRECTORY_SEPARATOR . 'STXINWEI.TTF'; 
 
+echo $font;die;
 imagejpeg($image);
 imagedestroy($image);
