bee6768bbdfa09f9db6fddfdd707f2c422407689
rjomg <1241718398@qq.com>
2018-04-09 17:58:08 +0800

    日志

--- member/memberlogs.php
+++ member/memberlogs.php
@@ -17,17 +17,20 @@
 		<input type="hidden" name="formhash" value="b718f6b0">	
 		<input type="hidden" name="docom" value="">	
 
-		<tr class="header_left_b"><td colspan="14">日志</td></tr>
+		<tr class="header_left_b">
+            <td colspan="6">
+                日志
+                <span id="logsclass" class="log_nav"><a href="JavaScript:;" onclick="cgLogs.LogsClass(this,1);">二定位</a>&nbsp;&nbsp;<a href="JavaScript:;" onclick="cgLogs.LogsClass(this,2);">三定位</a>&nbsp;&nbsp;<a href="JavaScript:;" onclick="cgLogs.LogsClass(this,3);">四定位</a>&nbsp;&nbsp;<a href="JavaScript:;" onclick="cgLogs.LogsClass(this,4);">二字现</a>&nbsp;&nbsp;<a href="JavaScript:;" onclick="cgLogs.LogsClass(this,5);">三字现</a>&nbsp;&nbsp;<a href="JavaScript:;" onclick="cgLogs.LogsClass(this,6);">四字现</a>&nbsp;&nbsp;<a href="JavaScript:;" onclick="cgLogs.LogsClass(this,0);" class="active" style="color: #fff800">全部</a>&nbsp;&nbsp;</span>
+            </td>
+        </tr>
 		<tr class="soon_head" > 
-		<td width="7%" >笔数</td>
-		<td width="7%"  >金额</td>
-		<td width="74%" >操作内容(日志是记录操作条件，方便核对，实际下单内容已下注明细为准）</td>
-		<td width="12%" >操作时间</td>
+            <td width="6%" >笔数</td>
+            <td width="6%"  >金额</td>
+            <td width="53%" >操作内容(日志是记录操作条件，方便核对，实际下单内容已下注明细为准</td>
+            <td width="9%" >操作时间</td>
+            <td width="9%" >再次生成</td>
+            <td width="5%" ><input class="btn_tuima" style="padding: 0 1px;line-height: 22px;;height: 22px;height: 22px !important;" type="button"  value="常用"></td>
 		</tr>
-
-				<tr><td colspan="17">还没有内容</td></tr>
-				 
-
 		</table>
 		<BR>
 		</form>
@@ -39,10 +42,15 @@
 	<table width="98%" align="center" border="0" cellpadding="0" cellspacing="0" >
 	<tr >
 	<td align="center" style="text-align:center">
-版权所有 Copyright@2009-2010 usetime:0.012830 
+版权所有 Copyright@2009-2010 usetime:0.012830
 mysqlquery:4 
 </td>
 <tr>
 </table> -->
 </body>
 </html>
+<!--<SCRIPT LANGUAGE="JavaScript">-->
+<!--    function LogsClass(that){-->
+<!--        $()-->
+<!--    }-->
+<!--</SCRIPT>-->
